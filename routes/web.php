b877a2c1d73d1e8cc7b6c21f85cb6c75efaa8245
Zakwan <fahdzakwan@gmail.com>
2021-04-26 13:05:50 +0600

    Initial Commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/questionnaires/create', 'App\Http\Controllers\QuestionnaireController@create');
+Route::post('/questionnaires', 'App\Http\Controllers\QuestionnaireController@store');
+Route::get('/questionnaires/{questionnaire}', 'App\Http\Controllers\QuestionnaireController@show');
